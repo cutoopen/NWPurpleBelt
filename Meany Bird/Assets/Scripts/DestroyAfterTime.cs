01c0f6e19854e8ee007765f9ceaed2711e977d3a
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-03-16 12:07:12 -0400

    rcytvubih

---
+++ Meany Bird/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    //After this time, the object will be destroyed
+    [Header("Default Destruction Time")]
+    public float timeToDestruction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Execute DestroyObject function based on timeToDestruction
+        Invoke("DestroyObject", timeToDestruction);
+
+    }
+    void DestroyObject()
+    {
+        //DestroyObject
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Meany Bird/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //Objects that we will attach to the script for spawning object
+    [Header("Spikes Object for controlling the game")]
+    public GameObject spikes;
+    //Height position of the spikes
+    [Header("Default Height")]
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Start function repeating every 4 seconds
+        InvokeRepeating("InstantiateObjects", 1f, 4f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Position for the game objects
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
+    }
+    void InstantiateObjects()
+    {
+        //Spawn objects by postion and rotation
+        Instantiate(spikes, transform.position, transform.rotation);
+    }
+}
