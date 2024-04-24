6e9c4447064a8df77ce9dce434962d386e155d0a
cutoopen <161525484+cutoopen@users.noreply.github.com>
2024-04-24 16:30:47 -0400

    ds
    sfd

---
+++ Sketch Head/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    //Platform gameobject
+    [Header("Platform Object")]
+    public GameObject platform;
+    //Default position for platform
+    float pos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <1000; i++)
+        {
+            //Execute platform
+            SpawnPlatforms();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnPlatforms()
+    {
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
+    }
+}
